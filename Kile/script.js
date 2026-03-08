d2a20c91c83230213205edeb3bb38d2e635b1606
KileWu1 <kile30302@gmail.com>
2026-03-08 21:12:17 +0800

    Add files via upload

--- Kile/script.js
+++ Kile/script.js
@@ -19,3 +19,59 @@ links.forEach(link => {
         navLinks.classList.remove('active');
     });
 });
+
+
+// Lightbox 放大檢視
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.lightbox .close');
+
+// 點擊圖片放大
+document.querySelectorAll('.gallery-item').forEach(item => {
+    item.addEventListener('click', () => {
+
+        // 如果是 PDF 不處理
+        if (item.closest('.pdf')) return;
+
+        lightbox.style.display = 'flex';
+        lightboxImg.src = item.src;
+    });
+});
+
+// 點 X 關閉
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
+// 點背景關閉
+lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) {
+        lightbox.style.display = 'none';
+    }
+});
+
+// ESC 關閉
+document.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        lightbox.style.display = 'none';
+    }
+});
+
+
+const container = document.getElementById("attachmentsContainer");
+const leftBtn = document.getElementById("scrollLeft");
+const rightBtn = document.getElementById("scrollRight");
+
+leftBtn.addEventListener("click", () => {
+    container.scrollBy({
+        left: -350,
+        behavior: "smooth"
+    });
+});
+
+rightBtn.addEventListener("click", () => {
+    container.scrollBy({
+        left: 350,
+        behavior: "smooth"
+    });
+});
